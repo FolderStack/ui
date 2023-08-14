c0f5af6461901d924e070bffbd674ac43f1dbe77
Brayden Phillips <brayden@Braydens-MacBook-Pro-2.local>
2023-08-14 16:36:03 +1000

    fixed config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,40 @@
+const env = String(
+    process.env.VERCEL_ENV ?? process.env.NODE_ENV
+).toLowerCase();
+
+function getApiUrl() {
+    switch (env) {
+        case "production":
+        case "prod":
+            return "https://api.folderstack.io/api/v1/";
+        case "staging":
+            return "https://staging-api.folderstack.io/api/v1";
+        case "development":
+        case "dev":
+        case "local":
+            return "http://localhost:4000";
+        default:
+            throw new Error("Bad environment configuration.");
+    }
+}
+
+function getHeaders() {
+    switch (env) {
+        case "development":
+        case "dev":
+        case "local":
+            return {
+                "X-Test-Authorizer":
+                    '{"orgId": "034b110d-f7d2-47c6-b90e-81a0062b04a0"}',
+            };
+        default:
+            return {};
+    }
+}
+
 export const config = {
     api: {
-        baseUrl: "http://localhost:4000",
-        headers: {
-            "X-Test-Authorizer":
-                '{"orgId": "034b110d-f7d2-47c6-b90e-81a0062b04a0"}',
-        },
+        baseUrl: getApiUrl(),
+        headers: getHeaders(),
     },
 };
